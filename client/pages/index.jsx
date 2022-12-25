bb02c889bc1b56a99db197b2bd75e58536f32637
Yaw Botwe <ybotwe@gmail.com>
2022-12-25 10:15:04 +0000

    removed link

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -246,7 +246,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://alchemy.com/?a=roadtoweb3weektwo"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
